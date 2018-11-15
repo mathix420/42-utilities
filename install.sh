a2e9bb7d87710d77530ec6b9edb811ea2470df15
Arnaud GISSINGER <agissing@e3r8p7.42.fr>
2018-11-15 10:33:42 +0100

    Install.sh Updated

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'alias mf-gen="python3 ~/42-utilities/mf-gen.py"' >> ~/.zshrc;
-echo 'alias proto-list="python3 ~/42-utilities/proto-list.py"' >> ~/.zshrc;
+[ "$(grep 'alias mf-gen=' ~/.zshrc)" ] && echo "Aliases already exists.\nPlease verify if they are exacts !" || echo 'alias mf-gen="python3 ~/42-utilities/mf-gen.py"' >> ~/.zshrc; echo 'alias proto-list="python3 ~/42-utilities/proto-list.py"' >> ~/.zshrc;
+
