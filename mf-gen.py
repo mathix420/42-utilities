2885b7f65bb0efb32ba25bf45f49e4da1ff9f1c5
Arnaud GISSINGER <agissing@e3r8p7.42.fr>
2018-11-15 15:32:09 +0100

    Makedepend done

--- mf-gen.py
+++ mf-gen.py
@@ -6,7 +6,7 @@
 #    By: agissing <agissing@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/10 18:01:56 by agissing          #+#    #+#              #
-#    Updated: 2018/11/15 11:58:17 by agissing         ###   ########.fr        #
+#    Updated: 2018/11/15 15:13:36 by agissing         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,14 +16,18 @@ def ft_putspc(str):
     return(' ' * (40 - len(str)))
 
 c = len(sys.argv)
+model_name = "MakeSimple"
 if (c == 1):
     prog = input('Program name : ')
     dir = input("Sources directory : ")
     inc = input("Headers directory : ")
-elif (c == 4):
+elif (c >= 4):
     prog = sys.argv[1]
     dir = sys.argv[2]
     inc = sys.argv[3]
+    for i in range(4, len(sys.argv)):
+        if (i + 1 < len(sys.argv) and sys.argv[i] == "--type"):
+            model_name = sys.argv[i + 1]
 else:
     sys.exit("Bad arguments !")
     
@@ -39,55 +43,7 @@ date = time.strftime('%Y/%m/%d %X') + ' by ' + user
 commande = "cd %s && ls *.c | sort | sed 's/$/ \\\/'" % dir
 files = subprocess.check_output(['bash','-c', commande]).decode()[:-2]
 
-header = '''# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: {0} <{1}>{2}+#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: {3}{4} #+#    #+#              #
-#    Updated: {3}{4}###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-'''
-
-text = header.format(user, mail, ft_putspc(user + mail),
-                     date, ft_putspc(date)) + '''
-NAME = {0}
-
-IDIR = {1}
-_INC = {0}.h
-INC = $(patsubst %,$(IDIR)/%,$(_INC))
-
-SDIR = {2}
-_SRC = {3}
-SRC = $(patsubst %,$(SDIR)/%,$(_SRC))
-
-OBJ = $(_SRC:.c=.o)
-
-FLAGS = -Wall -Wextra -Werror
-OPTS = -o $(NAME) -I$(IDIR)
-
-.PHONY: all clean fclean re
-
-all: $(NAME)
-
-$(NAME): $(OBJ)
-\t\t@gcc $(FLAGS) $(OPTS) $(SRC)
-
-$(OBJ): $(SRC)
-\t\t@gcc -c -I$(IDIR) $(SRC)
-
-clean:
-\t\t@/bin/rm -f $(OBJ)
-
-fclean: clean
-\t\t@/bin/rm -f $(NAME)
-
-re: clean all'''.format(prog, inc, dir, files)
-
-model = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'MakeDepend'), 'r')
+model = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), model_name), 'r')
 
 makefile.write(model.read().format(user, mail, ft_putspc(user + mail),
                      date, ft_putspc(date), prog, inc, dir, files))
