8c07801b30938e26d3f91b0630d6602b90bd6f6c
Mathix 420 <gissingerarnaud@gmail.com>
2019-02-01 23:34:27 +0100

    Big changes and fix #7

--- mf-gen.py
+++ mf-gen.py
@@ -6,47 +6,162 @@
 #    By: agissing <agissing@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/10 18:01:56 by agissing          #+#    #+#              #
-#    Updated: 2018/11/15 15:13:36 by agissing         ###   ########.fr        #
+#    Updated: 2019/02/01 23:23:56 by agissing         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import subprocess, time, sys, os
+import subprocess, time, sys, os, re
+from os import path
+
+app_path = path.dirname(path.realpath(__file__))
+PARAMS = ['-n', '--lib', '--and', '--type']
+OPTIONS = ['-S', '--sumary']
+
+objects = []
+
+class obj():
+    def __init__(self):
+        self.name = ''
+        self.dir = ''
+        self.inc = ''
+        self.lib = []
 
 def ft_putspc(str):
     return(' ' * (40 - len(str)))
 
-c = len(sys.argv)
+def get_userinfos():
+    try:
+        _mail = os.environ['MAIL']
+    except:
+        _mail = 'marvin@42.fr'
+    try:
+        _user = os.environ['USER']
+    except:
+        _user = 'marvin'
+    return (_user, _mail)
+    
+def check_args(argv):
+# Return 0 for gui > 3 for command line and < 0 if errors occurs
+    c = 0
+    n = 1
+    for arg in argv[1:]:
+        if arg in PARAMS:
+            if arg == '-n':
+                n = -1
+            c -= 2
+        elif arg in OPTIONS:
+            c -= 1
+        elif n == -1:
+            n = int(arg)
+        c += 1
+    return (c, n)
+
+c, n = check_args(sys.argv)
 model_name = "MakeSimple"
-if (c == 1):
-    prog = input('Program name : ')
-    dir = input("Sources directory : ")
-    inc = input("Headers directory : ")
-elif (c >= 4):
-    prog = sys.argv[1]
-    dir = sys.argv[2]
-    inc = sys.argv[3]
-    for i in range(4, len(sys.argv)):
-        if (i + 1 < len(sys.argv) and sys.argv[i] == "--type"):
-            model_name = sys.argv[i + 1]
+if (c == 0):
+    for i in range(n):
+        objects.append(obj())
+        objects[-1].name = input('Program name : ')
+        objects[-1].dir = input("Sources directory : ")
+        objects[-1].inc = input("Headers directory : ")
+        for l in input("Local librairies (<dirname>.<libname>) : ").split(' '):
+            if l != '':
+                objects[-1].lib.append(l.split('.'))
+    print ("\nNext time you can use this command :\n\n\t$ mf-gen", ' --and '.join([('%s "%s" "%s" ' % (o.name, o.dir, o.inc)) + ' '.join(['--lib %s.%s' % (l[0], l[1]) for l in o.lib]) for o in objects]))
+elif (c >= 3):
+    d = 0
+    for i in range(1, len(sys.argv)):
+        if d == 0:
+            objects.append(obj())
+            objects[-1].name = sys.argv[i]
+            d += 1
+        elif d == 1:
+            objects[-1].dir = sys.argv[i]
+            d += 1
+        elif d == 2:
+            objects[-1].inc = sys.argv[i]
+            d += 1
+        elif sys.argv[i] == "--and":
+            d = 0;
+        elif i > 0 and sys.argv[i - 1] == "--lib":
+            objects[-1].lib.append(sys.argv[i].split('.'))
+        for i in range(4, len(sys.argv)):
+            if (i + 1 < len(sys.argv) and sys.argv[i] == "--type"):
+                model_name = sys.argv[i + 1]
 else:
     sys.exit("Bad arguments !")
-    
+
+def header_42():
+    user, mail = get_userinfos()
+    date = time.strftime('%Y/%m/%d %X') + ' by ' + user
+    header_template = open(path.join(app_path, 'header_42'), 'r')
+    return (header_template.read().format(user, mail, ft_putspc(user + mail),
+                                          date, ft_putspc(date)))
+
 makefile = open('Makefile', 'w+')
 
-try:
-    mail = os.environ['MAIL']
-except:
-    mail = 'marvin@42.fr'
-user = os.environ['USER']
-date = time.strftime('%Y/%m/%d %X') + ' by ' + user
+output = header_42() + '''
+################################################################################
+#####                              FILES VARS                              #####
+################################################################################
 
-commande = "cd %s && ls *.c | sort | sed 's/$/ \\\/'" % dir
-files = subprocess.check_output(['bash','-c', commande]).decode()[:-2]
+'''
 
-model = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), model_name), 'r')
+def beautify(str):
+    return (re.compile('[\W_]+').sub('', str).upper())
 
-makefile.write(model.read().format(user, mail, ft_putspc(user + mail),
-                     date, ft_putspc(date), prog, inc, dir, files))
+for prog in objects:
+    output += beautify(prog.name) + ' = ' + prog.name + '\n'
+for prog in objects:
+    end = beautify(prog.name) + '_SRC ='
+    i = objects.index(prog)
+    for d in prog.dir.split(' '):
+        commande = "cd %s && ls *.c | sort | sed 's/$/ \\\/'" % d
+        files = subprocess.check_output(['bash','-c', commande]).decode()[:-2]
+        output += "\nDIR_" + beautify(d) + str(i) + ' = ' + d + '\n'
+        output += "_" + beautify(d) + str(i) + ' = ' + files + '\n'
+        end += ' $(patsubst %,$(' + "DIR_" + beautify(d) + str(i)
+        end += ')/%,$(' + "_" + beautify(d) + str(i) + '))'
+    output += end + '\n'
+    output += '\n{0}_OBJ = $({0}_SRC:.c=.o)\n'.format(beautify(prog.name))
+
+output += '''
+################################################################################
+#####                           COMPILER OPTIONS                           #####
+################################################################################
+
+CC = gcc
+FLAGS = -Wall -Wextra -Werror
+
+################################################################################
+#####                            MAKEFILE RULES                            #####
+################################################################################
+
+.PHONY: all clean fclean re
+
+all :'''
+
+for o in objects:
+    output += ' $(%s)' % beautify(o.name)
+
+output += '\n\n'
+
+for prog in objects:
+    output += '$({0}_OBJ) : %.o : %.c\n'.format(beautify(prog.name))
+    output += '\t@$(CC) $(FLAGS) -c $< -o $@ '
+    output += ' '.join(['-I %s' % n for n in prog.inc.split(' ')]) + '\n\n'
+    output += '$({0}) : $({0}_OBJ)\n\t@$(CC) $(FLAGS) $({0}_OBJ) -o $@ '.format(beautify(prog.name))
+    output += ' '.join(['-I %s' % n for n in prog.inc.split(' ')]) + ' '
+    if len(prog.lib) > 0:
+        output += ' '.join(['-L%s -l%s' % (v[0], v[1].lstrip('lib')) for v in prog.lib])
+    output += '\n\n'
+output += 'clean :\n\t@/bin/rm -f '
+output += ' '.join(['$(%s_OBJ)' % beautify(o.name) for o in objects]) + '\n\n'
+output += 'fclean : clean\n\t@/bin/rm -f '
+output += ' '.join(['$(%s)' % beautify(o.name) for o in objects]) + '\n\n'
+output += 're : fclean all\n'
+
+makefile.write(output)
 
 if (c == 1):
     print ("All done !")
